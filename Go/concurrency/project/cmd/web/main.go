ae0f48e9b3986579c3fece954e092f92845ea050
mstgnz <mesutgenez@hotmail.com>
2023-12-20 22:59:50 +0300

    last

--- Go/concurrency/project/cmd/web/main.go
+++ Go/concurrency/project/cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/gob"
+	"final-project/data"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,8 +12,6 @@ import (
 	"sync"
 	"syscall"
 	"time"
-
-	"project/data"
 
 	"github.com/alexedwards/scs/redisstore"
 	"github.com/alexedwards/scs/v2"
@@ -48,15 +47,33 @@ func main() {
 		ErrorLog: errorLog,
 		Wait:     &wg,
 		Models:   data.New(db),
+		ErrorChan: make(chan error),
+		ErrorChanDone: make(chan bool),
 	}
 
 	// set up mail
+	app.Mailer = app.createMail()
+	go app.listenForMail()
 
 	// listen for signals
 	go app.listenForShutdown()
 
+	// listen for errors
+	go app.listenForErrors()
+
 	// listen for web connections
 	app.serve()
+}
+
+func (app *Config) listenForErrors() {
+	for {
+		select {
+		case err := <-app.ErrorChan:
+			app.ErrorLog.Println(err)
+		case <-app.ErrorChanDone:
+			return
+		}
+	}
 }
 
 func (app *Config) serve() {
@@ -129,7 +146,7 @@ func openDB(dsn string) (*sql.DB, error) {
 // initSession sets up a session, using Redis for session store
 func initSession() *scs.SessionManager {
 	gob.Register(data.User{})
-
+	
 	// set up session
 	session := scs.New()
 	session.Store = redisstore.New(initRedis())
@@ -169,5 +186,35 @@ func (app *Config) shutdown() {
 	// block until waitgroup is empty
 	app.Wait.Wait()
 
+	app.Mailer.DoneChan <- true
+	app.ErrorChanDone <- true
+
 	app.InfoLog.Println("closing channels and shutting down application...")
+	close(app.Mailer.MailerChan)
+	close(app.Mailer.ErrorChan)
+	close(app.Mailer.DoneChan)
+	close(app.ErrorChan)
+	close(app.ErrorChanDone)
+}
+
+func (app *Config) createMail() Mail {
+	// create channels
+	errorChan := make(chan error)
+	mailerChan := make(chan Message, 100)
+	mailerDoneChan := make(chan bool)
+
+	m := Mail{
+		Domain: "localhost",
+		Host: "localhost",
+		Port: 1025,
+		Encryption: "none",
+		FromName: "Info",
+		FromAddress: "info@mycompany.com",
+		Wait: app.Wait,
+		ErrorChan: errorChan,
+		MailerChan: mailerChan,
+		DoneChan: mailerDoneChan,
+	}
+
+	return m
 }
--- Go/concurrency/project/db.sql
+++ Go/concurrency/project/db.sql
@@ -2,13 +2,12 @@
 -- Name: plans; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.plans
-(
-    id          integer NOT NULL,
-    plan_name   character varying(255),
-    plan_amount integer,
-    created_at  timestamp without time zone,
-    updated_at  timestamp without time zone
+CREATE TABLE public.plans (
+                              id integer NOT NULL,
+                              plan_name character varying(255),
+                              plan_amount integer,
+                              created_at timestamp without time zone,
+                              updated_at timestamp without time zone
 );
 
 
@@ -16,15 +15,14 @@ CREATE TABLE public.plans
 -- Name: plans_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-ALTER TABLE public.plans
-    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-        SEQUENCE NAME public.plans_id_seq
-        START WITH 1
-        INCREMENT BY 1
-        NO MINVALUE
-        NO MAXVALUE
-        CACHE 1
-        );
+ALTER TABLE public.plans ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.plans_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 
 --
@@ -43,13 +41,12 @@ CREATE SEQUENCE public.user_id_seq
 -- Name: user_plans; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.user_plans
-(
-    id         integer NOT NULL,
-    user_id    integer,
-    plan_id    integer,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+CREATE TABLE public.user_plans (
+                                   id integer NOT NULL,
+                                   user_id integer,
+                                   plan_id integer,
+                                   created_at timestamp without time zone,
+                                   updated_at timestamp without time zone
 );
 
 
@@ -57,35 +54,32 @@ CREATE TABLE public.user_plans
 -- Name: user_plans_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-ALTER TABLE public.user_plans
-    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-        SEQUENCE NAME public.user_plans_id_seq
-        START WITH 1
-        INCREMENT BY 1
-        NO MINVALUE
-        NO MAXVALUE
-        CACHE 1
-        );
-
-
-CREATE TABLE public.users
-(
-    id          integer DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
-    email       character varying(255),
-    first_name  character varying(255),
-    last_name   character varying(255),
-    password    character varying(60),
-    user_active integer DEFAULT 0,
-    is_admin    integer default 0,
-    created_at  timestamp without time zone,
-    updated_at  timestamp without time zone
+ALTER TABLE public.user_plans ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.user_plans_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
 );
 
 
-INSERT INTO "public"."users"("email", "first_name", "last_name", "password", "user_active", "is_admin", "created_at",
-                             "updated_at")
-VALUES (E'admin@example.com', E'Admin', E'User', E'$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe', 1, 1,
-        E'2022-03-14 00:00:00', E'2022-03-14 00:00:00');
+CREATE TABLE public.users (
+                              id integer DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
+                              email character varying(255),
+                              first_name character varying(255),
+                              last_name character varying(255),
+                              password character varying(60),
+                              user_active integer DEFAULT 0,
+                              is_admin integer default 0,
+                              created_at timestamp without time zone,
+                              updated_at timestamp without time zone
+);
+
+
+INSERT INTO "public"."users"("email","first_name","last_name","password","user_active", "is_admin", "created_at","updated_at")
+VALUES
+    (E'admin@example.com',E'Admin',E'User',E'$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe',1,1,E'2022-03-14 00:00:00',E'2022-03-14 00:00:00');
 
 SELECT pg_catalog.setval('public.plans_id_seq', 1, false);
 
@@ -95,10 +89,11 @@ SELECT pg_catalog.setval('public.user_id_seq', 2, true);
 
 SELECT pg_catalog.setval('public.user_plans_id_seq', 1, false);
 
-INSERT INTO "public"."plans"("plan_name", "plan_amount", "created_at", "updated_at")
-VALUES (E'Bronze Plan', 1000, E'2022-05-12 00:00:00', E'2022-05-12 00:00:00'),
-       (E'Silver Plan', 2000, E'2022-05-12 00:00:00', E'2022-05-12 00:00:00'),
-       (E'Gold Plan', 3000, E'2022-05-12 00:00:00', E'2022-05-12 00:00:00');
+INSERT INTO "public"."plans"("plan_name","plan_amount","created_at","updated_at")
+VALUES
+    (E'Bronze Plan',1000,E'2022-05-12 00:00:00',E'2022-05-12 00:00:00'),
+    (E'Silver Plan',2000,E'2022-05-12 00:00:00',E'2022-05-12 00:00:00'),
+    (E'Gold Plan',3000,E'2022-05-12 00:00:00',E'2022-05-12 00:00:00');
 
 
 ALTER TABLE ONLY public.plans
@@ -114,11 +109,11 @@ ALTER TABLE ONLY public.users
 
 
 ALTER TABLE ONLY public.user_plans
-    ADD CONSTRAINT user_plans_plan_id_fkey FOREIGN KEY (plan_id) REFERENCES public.plans (id) ON UPDATE RESTRICT ON DELETE CASCADE;
+    ADD CONSTRAINT user_plans_plan_id_fkey FOREIGN KEY (plan_id) REFERENCES public.plans(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
 
 ALTER TABLE ONLY public.user_plans
-    ADD CONSTRAINT user_plans_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users (id) ON UPDATE RESTRICT ON DELETE CASCADE;
+    ADD CONSTRAINT user_plans_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
 
 
